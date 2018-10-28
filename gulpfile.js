91ed0c4f9903e72694b82abe720d0ab9eb190fa3
Michael Read <michaelread1208@gmail.com>
2018-10-28 15:41:58 -0500

    Upgraded to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -15,42 +15,35 @@ const messages = {
   jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
-/**
- * Build the Jekyll Site
- */
-gulp.task('jekyll-build', done => {
+var paths = {
+  styles: {
+    src: '_scss/main.scss',
+    dest: '_site/assets/css',
+    destsecond: 'assets/css'
+  },
+  scripts: {
+    src: 'assets/scripts/*.js',
+    dest: '_site/assets/scripts'
+  }
+};
+
+function jekyllBuild() {
+  browserSync.reload();
   browserSync.notify(messages.jekyllBuild);
   if (env === 'prod') {
-    return cp.spawn(jekyll, ['build'], { stdio: 'inherit' }).on('close', done);
+    return cp.spawn(jekyll, ['build'], { stdio: 'inherit' });
   } else {
-    return cp
-      .spawn(jekyll, ['build', '--config', '_config.yml,_config.dev.yml'], {
+    return cp.spawn(
+      jekyll,
+      ['build', '--config', '_config.yml,_config.dev.yml'],
+      {
         stdio: 'inherit'
-      })
-      .on('close', done);
+      }
+    );
   }
-});
+}
 
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', ['jekyll-build'], () => browserSync.reload());
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], () => {
-  browserSync({
-    server: {
-      baseDir: '_site'
-    }
-  });
-});
-
-/**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
- */
-gulp.task('sass', () => {
+function style() {
   const processors = [
     prefix({ browsers: ['> 5%', 'last 3 versions'] }),
     csswring,
@@ -65,17 +58,25 @@ gulp.task('sass', () => {
       })
     )
     .pipe(postcss(processors))
-    .pipe(gulp.dest('_site/assets/css'))
+    .pipe(gulp.dest(paths.styles.dest))
     .pipe(browserSync.reload({ stream: true }))
-    .pipe(gulp.dest('assets/css'));
-});
+    .pipe(gulp.dest(paths.styles.destsecond));
+}
 
-/**
- * Watch scss files for changes & recompile
- * Watch html/md/js/image/json files, run jekyll & reload BrowserSync
- */
-gulp.task('watch', () => {
-  gulp.watch('_scss/*.scss', ['sass']);
+function reload() {
+  browserSync.reload();
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: '_site'
+    }
+  });
+  gulp.watch('_scss/*.scss', style);
+  // We should tell gulp which files to watch to trigger the reload
+  // This can be html or whatever you're using to develop your website
+  // Note -- you can obviously add the path to the Paths object
   gulp.watch(
     [
       './**/*.html',
@@ -84,9 +85,69 @@ gulp.task('watch', () => {
       'assets/images/*.*',
       './**/*.md'
     ],
-    ['jekyll-rebuild']
+    gulp.series(jekyllBuild, reload)
   );
-});
+}
+
+/**
+ * Build the Jekyll Site
+ */
+// gulp.task("jekyll-build", () => {
+// 	browserSync.notify(messages.jekyllBuild);
+// 	if (env === "prod") {
+// 		return cp.spawn(jekyll, ["build"], { stdio: "inherit" });
+// 	} else {
+// 		return cp.spawn(jekyll, ["build", "--config", "_config.yml,_config.dev.yml"], {
+// 			stdio: "inherit"
+// 		});
+// 	}
+// });
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+// gulp.task("jekyll-rebuild", gulp.series("jekyll-build", reload));
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+// gulp.task(
+// 	"browser-sync",
+// 	gulp.series(style, "jekyll-build", () => {
+// 		browserSync({
+// 			server: {
+// 				baseDir: "_site"
+// 			}
+// 		});
+// 	})
+// );
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+// gulp.task(
+// 	"sass",
+// 	gulp.series("jekyll-build", () => {
+// 		const processors = [prefix({ browsers: ["> 5%", "last 3 versions"] }), csswring, cssnano];
+// 		return gulp
+// 			.src("_scss/main.scss")
+// 			.pipe(
+// 				sass({
+// 					includePaths: ["scss"],
+// 					onError: browserSync.notify
+// 				})
+// 			)
+// 			.pipe(postcss(processors))
+// 			.pipe(gulp.dest("_site/assets/css"))
+// 			.pipe(browserSync.reload({ stream: true }))
+// 			.pipe(gulp.dest("assets/css"));
+// 	})
+// );
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md/js/image/json files, run jekyll & reload BrowserSync
+ */
 
 /**
  * Delete .publish directory
@@ -99,11 +160,13 @@ gulp.task('clean', () => del('.publish/**/*'));
  * To run locally:
  * $ NODE_ENV=dev gulp
  */
-gulp.task('default', ['browser-sync', 'watch']);
+// gulp.task("default", gulp.series(watch));
+gulp.task('default', gulp.series(jekyllBuild, watch));
 
 /**
  * Deploy to GitHub Pages
  */
-gulp.task('deploy', ['jekyll-build'], () =>
+// gulp.task("deploy", gulp.series("jekyll-build"), () => gulp.src("./_site/**/*").pipe(deploy()));
+gulp.task('deploy', gulp.parallel(jekyllBuild), () =>
   gulp.src('./_site/**/*').pipe(deploy())
 );
